f6acc972487dea43adcb812236c256d2d21c1799
magb7 <margaux.bedu@gmail.com>
2020-06-14 19:11:28 +0200

    import screens

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import { Text, TouchableWithoutFeedback, View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <TouchableWithoutFeedback>
-        <Text>Hello React Native</Text>
-      </TouchableWithoutFeedback>
-    </View>
-  );
+  return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
