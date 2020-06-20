92c48cceb508906773fac0aed936d3ecea5baa1a
magb7 <margaux.bedu@gmail.com>
2020-06-20 19:06:57 +0200

    display card component while building

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { Text, TouchableWithoutFeedback, View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
+
+import Card from "./app/components/Card";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
