0f83fd8be0aec82d123abf79ea843ef6ee3c6d79
magb7 <margaux.bedu@gmail.com>
2020-06-20 18:30:40 +0200

    import custom buttons and change some styles on background image and tagline

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { StyleSheet, View, ImageBackground, Image, Text } from "react-native";
 
+import AppButton from "../components/AppButton";
 import colors from "../config/colors";
+import App from "../../App";
 
 const WelcomeScreen = () => {
   return (
     <ImageBackground
+      blurRadius={2}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonContainer}>
+        <AppButton
+          title="Login"
+          onPress={() => {
+            console.log("Tapped");
+          }}
+        />
+        <AppButton title="Register" color="secondary" />
+      </View>
     </ImageBackground>
   );
 };
@@ -27,10 +37,9 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
   },
-  loginButton: {
+  buttonContainer: {
+    padding: 20,
     width: "100%",
-    height: 70,
-    backgroundColor: colors.primary,
   },
   logo: {
     width: 100,
@@ -41,9 +50,9 @@ const styles = StyleSheet.create({
     top: 70,
     alignItems: "center",
   },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: colors.secondary,
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
 });
