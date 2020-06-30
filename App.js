63a6a1f21c538d54279e299bef3d9e0d7a5c2bc5
magb7 <margaux.bedu@gmail.com>
2020-06-30 18:53:43 +0200

    create new components for message display

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
 import React from "react";
-import { View } from "react-native";
-
-import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <MessagesScreen />;
 }
