7c81f88e1bc1013786528d0631c41a8327de29da
magb7 <margaux.bedu@gmail.com>
2020-06-30 18:52:40 +0200

    add padding to the container

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -19,6 +19,7 @@ const ListItem = ({ title, subTitle, image }) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    padding: 15,
   },
   image: {
     width: 70,
