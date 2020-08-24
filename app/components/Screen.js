b8249e5e395d9c707aa436f41ca8a88e10b4c8f3
magb7 <margaux.bedu@gmail.com>
2020-08-24 16:00:01 +0200

    add style props to overwrite default style of the component

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -2,8 +2,8 @@ import React from "react";
 import Constants from "expo-constants";
 import { StyleSheet, SafeAreaView } from "react-native";
 
-const Screen = ({ children }) => {
-  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+const Screen = ({ children, style }) => {
+  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
